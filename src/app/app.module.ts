fa18c5ae635f55848194261bb13c639cd97e753b
Jonathan Romero Castillo <n.bjhon@gmail.com>
2020-02-05 07:41:01 -0500

    añadiendo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//Rutas
+import { APP_ROUTES } from './app-routing.module';
+
+//Servicios
+import { HeroesService } from './servicios/heroes.service';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { RouterModule } from '@angular/router';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(APP_ROUTES)
   ],
-  providers: [],
+  providers: [ HeroesService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
